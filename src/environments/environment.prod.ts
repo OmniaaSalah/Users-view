a330b705fc0097668629545e7279017420a1197e
MOSTAPHA <mac@MacBook-Pro-de-mac.local>
2022-08-01 23:58:00 +0100

    app generation

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  appName: 'Daleel System',
+  production: true,
+  serverUrl: "https://mrvms.azurewebsites.net/api",
+  env: 'Development',
+  version: '1.0',
+  defaultLang: 'en'
+};
