3c8f6257547d3c399d895282116fabec85a316c8
MOSTAPHA <mac@MacBook-Pro-de-mac.local>
2022-08-15 01:01:52 +0100

    translation-module-and-http-service-setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './core/services/auth-guard.service';
-import { TokenGuard } from './core/services/token-guard.service';
+// import { AuthGuard } from './core/services/auth-guard.service';
+import { AuthenticationGuard } from './core/services/authentication.guard';
+// import { TokenGuard } from './core/services/token-guard.service';
 
 const routes: Routes = [
   {
@@ -10,7 +11,6 @@ const routes: Routes = [
       import('./modules/authentication/authentication.module').then(
         (a) => a.AuthenticationModule
       ),
-    canActivate: [TokenGuard],
   },
   {
     path: 'schools',
@@ -18,7 +18,7 @@ const routes: Routes = [
       import('./modules/schools/schools.module').then(
         (a) => a.SchoolsModule
       ),
-    canActivate: [AuthGuard],
+    canActivate: [AuthenticationGuard],
   },
 
   {
@@ -28,7 +28,7 @@ const routes: Routes = [
   },
 
   {
-    path: 'schools', 
+    path: 'schools',
     loadChildren: () => import('./modules/schools/schools.module').then((m) => m.SchoolsModule)
   },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { SchoolsModule } from './modules/schools/schools.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
-import { AuthGuard } from './core/services/auth-guard.service';
-import { TokenGuard } from './core/services/token-guard.service';
+// import { AuthGuard } from './core/services/auth-guard.service';
+// import { TokenGuard } from './core/services/token-guard.service';
 import { LayoutModule } from './layout/layout.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -27,24 +27,17 @@ import { LayoutModule } from './layout/layout.module';
     BrowserAnimationsModule,
     SharedModule,
     LayoutModule,
-    TranslateModule.forRoot({
-      defaultLanguage: 'ar',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
+    TranslateModule.forRoot(),
     BrowserAnimationsModule
   ],
   providers: [
-    AuthGuard,
-    TokenGuard,
+    // AuthGuard,
+    // TokenGuard,
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json')
-}
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json')
+// }
