a0ade262c6e0d9d2b49b77b02913ec274c83be54
Omnia-Salah <omnia.salah@valsquad.com>
2022-09-21 13:37:08 +0200

    update schoolyear component

---
+++ src/app/shared/components/add-btn/add-btn.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-add-btn',
+  templateUrl: './add-btn.component.html',
+  styleUrls: ['./add-btn.component.scss']
+})
+export class AddBtnComponent implements OnInit {
+  @Input('route') routeUrl='';
+  @Input('content') content='';
+  plusIcon = faPlus;
+  constructor( private router: Router,private route:ActivatedRoute) { }
+  
+  ngOnInit(): void {
+  }
+
+  
+  goToAddNew()
+  {
+   
+    this.router.navigate([this.routeUrl],{relativeTo:this.route});
+  }
+
+
+}
