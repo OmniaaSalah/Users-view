04b756b6b429ff0d19a44989edc6ebba7964b4d4
mahmoudibrahim7 <mahmoud.ibrahim@valsquad.com>
2022-10-03 16:41:59 +0200

    upload ui of ceritficate

--- src/app/modules/dashboard/modules/students/students.module.ts
+++ src/app/modules/dashboard/modules/students/students.module.ts
@@ -13,6 +13,7 @@ import { RegisterStudentComponent } from './components/register-student/register
 import { TransferStudentComponent } from './components/transfer-student/transfer-student.component';
 import { StudentMedicalFileComponent } from './components/student-medical-file/student-medical-file.component';
 import { DashboardSharedModule } from '../shared/dashboard-shared/dashboard-shared.module';
+import { IssuanceOfACertificateComponent } from './components/issuance-of-a-certificate/issuance-of-a-certificate.component';
 
 
 
@@ -23,7 +24,8 @@ import { DashboardSharedModule } from '../shared/dashboard-shared/dashboard-shar
     StudentDetailsComponent,
     RegisterStudentComponent,
     TransferStudentComponent,
-    StudentMedicalFileComponent
+    StudentMedicalFileComponent,
+    IssuanceOfACertificateComponent
   ],
   imports: [
     CommonModule,
