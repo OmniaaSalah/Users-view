5693c2f8608944334c581d85a5c8663078aade3c
mahmoudibrahim7 <mahmoud.ibrahim@valsquad.com>
2022-12-01 17:07:15 +0200

    binind object and array with translate and do some function to finalize parent requests

---
+++ src/app/modules/dashboard/modules/request-list/services/parent-request.service.ts
@@ -0,0 +1,108 @@
+import { of, finalize, take } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { LoaderService } from 'src/app/shared/services/loader/loader.service';
+import { Filter } from 'src/app/core/models/filter/filter';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParentRequestService {
+
+  constructor() { }
+
+   requestArray = {
+    "totalAllData": 7,
+    "total": 7,
+    "data":[
+      {
+        "id": "1001",
+        "code": "nvklal433",
+        "name": "Black Watch",
+        "description": "Product Description",
+        "image": "black-watch.jpg",
+        "price": 72,
+        "category": "Accessories",
+        "quantity": 61,
+        "inventoryStatus": "INSTOCK",
+        "rating": 4
+      },
+      {
+        "id": "1001",
+        "code": "nvklal433",
+        "name": "Black Watch",
+        "description": "Product Description",
+        "image": "black-watch.jpg",
+        "price": 72,
+        "category": "Accessories",
+        "quantity": 61,
+        "inventoryStatus": "INSTOCK",
+        "rating": 4
+      },
+      {
+        "id": "1000",
+        "code": "f230fh0g3",
+        "name": "Bamboo Watch",
+        "description": "Product Description",
+        "image": "bamboo-watch.jpg",
+        "price": 65,
+        "category": "Accessories",
+        "quantity": 24,
+        "inventoryStatus": "INSTOCK",
+        "rating": 5
+      },
+      {
+        "id": "1001",
+        "code": "nvklal433",
+        "name": "Black Watch",
+        "description": "Product Description",
+        "image": "black-watch.jpg",
+        "price": 72,
+        "category": "Accessories",
+        "quantity": 61,
+        "inventoryStatus": "INSTOCK",
+        "rating": 4
+      },
+      {
+        "id": "1000",
+        "code": "f230fh0g3",
+        "name": "Bamboo Watch",
+        "description": "Product Description",
+        "image": "bamboo-watch.jpg",
+        "price": 65,
+        "category": "Accessories",
+        "quantity": 24,
+        "inventoryStatus": "INSTOCK",
+        "rating": 5
+      },
+      {
+        "id": "1001",
+        "code": "nvklal433",
+        "name": "Black Watch",
+        "description": "Product Description",
+        "image": "black-watch.jpg",
+        "price": 72,
+        "category": "Accessories",
+        "quantity": 61,
+        "inventoryStatus": "INSTOCK",
+        "rating": 4
+      },
+      {
+        "id": "1002",
+        "code": "zz21cz3c1",
+        "name": "Blue Band",
+        "description": "Product Description",
+        "image": "blue-band.jpg",
+        "price": 79,
+        "category": "Fitness",
+        "quantity": 2,
+        "inventoryStatus": "LOWSTOCK",
+        "rating": 3
+      },
+    ]
+  }
+
+  getRequests(filter?:Partial<Filter>){
+   return of(this.requestArray)
+  }
+
+}
