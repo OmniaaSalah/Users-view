d4ad2d91504b1bf1ac966d4c30c4cd77dd064221
mahmoudibrahim7 <mahmoud.ibrahim@valsquad.com>
2023-01-03 18:28:55 +0200

    update reports and update transfer-group

--- src/app/modules/dashboard/modules/reports-managment/reports-managment-routing.module.ts
+++ src/app/modules/dashboard/modules/reports-managment/reports-managment-routing.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DegreesReportsComponent } from './components/degrees-reports/degrees-reports.component';
 import { StudentsReportsComponent } from './components/students-reports/students-reports.component';
+import { SubjectsReportsComponent } from './components/subjects-reports/subjects-reports/subjects-reports.component';
+import { UsersReportsComponent } from './components/users-reports/users-reports/users-reports.component';
+import { AttendanceReportsComponent } from './components/attendance-reports/attendance-reports/attendance-reports.component';
 
 const routes: Routes = [
 
   {path:'', children:[
     {path: 'students-reports', component:StudentsReportsComponent},
-    {path: 'degrees-reports', component:DegreesReportsComponent}
+    {path: 'degrees-reports', component:DegreesReportsComponent},
+    {path: 'subjects-reports', component:SubjectsReportsComponent},
+    {path: 'users-reports', component:UsersReportsComponent},
+    {path: 'attendance-reports', component:AttendanceReportsComponent},
+
+
+
   ]}
 ];
 
--- src/app/modules/dashboard/modules/reports-managment/reports-managment.module.ts
+++ src/app/modules/dashboard/modules/reports-managment/reports-managment.module.ts
@@ -7,12 +7,18 @@ import { DegreesReportsComponent } from './components/degrees-reports/degrees-re
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PrimngModule } from 'src/app/modules/primng/primng.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { SubjectsReportsComponent } from './components/subjects-reports/subjects-reports/subjects-reports.component';
+import { UsersReportsComponent } from './components/users-reports/users-reports/users-reports.component';
+import { AttendanceReportsComponent } from './components/attendance-reports/attendance-reports/attendance-reports.component';
 
 
 @NgModule({
   declarations: [
     StudentsReportsComponent,
-    DegreesReportsComponent
+    DegreesReportsComponent,
+    SubjectsReportsComponent,
+    UsersReportsComponent,
+    AttendanceReportsComponent
   ],
   imports: [
     CommonModule,
