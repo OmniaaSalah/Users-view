962d66dbf791bb311034790b8b157e87a6a3d538
Ahmed Elsayyad <ahmadelsayyad2@gmail.com>
2023-05-15 18:55:47 +0300

    applay filter on children list in certificates pages

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -477,7 +477,6 @@ onScroll()
   unReadedMessagesCount=0
 
   getCurrentUserMessages(){
-    console.log("omn")
     let userId = this.userService.getCurrentUserId();
     this.messageService.getCurrentUserMessages(userId, this.currentUserScope,{PageSize:50, Page:1})
     .subscribe(res=>{
