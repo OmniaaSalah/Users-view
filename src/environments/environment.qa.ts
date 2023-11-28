ab0a621c880c977ef77e278a720cc99d3468127d
Ahmed Elsayyad <ahmadelsayyad2@gmail.com>
2023-11-28 10:19:22 +0200

    update version

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -3,12 +3,12 @@ import packageJson from '../../package.json';
 export const environment = {
   appName: 'Daleel System',
   production: true,
-  serverUrl: "https://api-daleel.spea.shj.ae/api",
-  env: 'Production',
+  serverUrl: "https://daleel-qa-api.azurewebsites.net/api",
+  env: 'QA',
   version: packageJson.version,
   defaultLang: 'ar',
-  logoutRedirectUrl: "https://daleel.spea.shj.ae/auth/login",
-  clientUrl:"https://daleel.spea.shj.ae/",
-  UAEPassLogout:"https://id.uaepass.ae/idshub/logout/"
+  logoutRedirectUrl: 'https://daleel-qa-app.azurewebsites.net/auth/login',
+  clientUrl:"https://daleel-qa-app.azurewebsites.net",
+  UAEPassLogout:"https://stg-id.uaepass.ae/idshub/logout/"
 
 };
